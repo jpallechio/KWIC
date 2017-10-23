ee37a802ab5213e53a9d419d952458a41f4a1b10
jpallechio <defiance018@gmail.com>
2017-10-23 12:22:34 -0700

    added files for assignment

---
+++ src/main/java/Line.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Line {
+    LinkedList<String> line = new LinkedList<>();
+
+    public void addWord(String string){}
+    public void shiftLine(){}
+    public int compareTo(){ return 0;}
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    private ArrayList<Line> lines;
+    public static void main(String[] args) {
+        //call readFile
+        //call
+    }
+    private void sortLines(Line[] lines){}
+    public void shitAllLines(Line[] lines){}
+    public void readFile(String filename){}
+    private void printLines(){}
+}
