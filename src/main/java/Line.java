53e21fa53717415f61a4b3d3c4a555a109140895
jpallechio <defiance018@gmail.com>
2017-10-23 14:06:43 -0700

    finished first design

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,9 +1,31 @@
 import java.util.LinkedList;
 
-public class Line {
+public class Line implements Comparable<Line> {
     LinkedList<String> line = new LinkedList<>();
 
-    public void addWord(String string){}
-    public void shiftLine(){}
-    public int compareTo(){ return 0;}
+    //add a work to the end of the linked list
+    public void addWord(String word){
+        line.add(word);
+    }
+    //shift the first word to the back
+    public void shiftLine(){
+        String firstWord = line.poll();
+        line.add(firstWord);
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        LinkedList<String> tempLine = new LinkedList<String>(line);
+        result += tempLine.pop();
+        while(tempLine.size() > 0){
+            result += " " + tempLine.pop();
+        }
+        return result;
+    }
+
+    @Override
+    public int compareTo(Line o) {
+        return this.toString().compareTo(o.toString());
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,86 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-    private ArrayList<Line> lines;
+    private static ArrayList<Line> lines = new ArrayList<>();
+
     public static void main(String[] args) {
         //call readFile
-        //call
+        readFile("inputTest.txt");
+
+        //call shiftAllLines
+        shiftAllLines(lines);
+
+        //sort all lines
+        sortLines(lines);
+
+        //call print lines
+        printLines();
     }
-    private void sortLines(Line[] lines){}
-    public void shitAllLines(Line[] lines){}
-    public void readFile(String filename){}
-    private void printLines(){}
+
+    private static void sortLines(ArrayList<Line> lines) {
+        Collections.sort(lines);
+    }
+
+    public static void shiftAllLines(ArrayList<Line> lines) {
+        for (Line line : lines) {
+            line.shiftLine();
+        }
+    }
+
+    public static void readFile(String filename) {
+        File file = new File(filename);
+
+        try {
+
+            Scanner sc = new Scanner(file);
+
+            //loop through the file, line by line
+            while (sc.hasNextLine()) {
+                //string of new line
+                String newLine = sc.nextLine();
+
+                //linked list for new line
+                Line line = new Line();
+
+                //loop through new line, adding it word by word
+                Scanner scanner = new Scanner(newLine);
+                while(scanner.hasNext()){
+                    line.addWord(scanner.next());
+                }
+                lines.add(line);
+                scanner.close();
+
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printLines(){
+        printToConsole();
+        printToFile("outputTest.txt");
+    }
+
+    private static void printToConsole(){
+        for (Line line : lines) {
+            System.out.println(line.toString());
+        }
+    }
+
+    private static void printToFile(String filename){
+        try {
+            PrintWriter pw = new PrintWriter(filename);
+            for (Line line : lines){
+                pw.println(line.toString());
+            }
+            pw.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
